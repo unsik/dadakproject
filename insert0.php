5dae73e3714d9d08dbb20533c9b913ead8fa1e38
unsik <fdew@naver.com>
2016-07-24 20:51:05 +0900

    Create insert0.php

---
+++ insert0.php
@@ -0,0 +1,43 @@
+<?php  
+$con=mysqli_connect("127.0.0.1","root","next1004","data");  
+ 
+mysqli_set_charset($con,"utf8");
+  
+if (mysqli_connect_errno($con))  
+{  
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();  
+}  
+$name = $_POST['name'];  
+$id = $_POST['id']; 
+$score = $_POST['score'];
+$time = $_POST['time'];  
+$mode = $_POST['mode'];  
+$result = array(); 
+  
+mysqli_query($con,"insert into Person (name,id,score,install) values ('$name','$id','$score','$time')");
+if($mode==2)
+{
+ $idcheck = mysqli_query($con,"UPDATE Person SET login='$time' where id = '$id'");
+}
+$idcheck = mysqli_query($con,"UPDATE Person SET lasttime='$time' where id = '$id'");
+if($idcheck)
+{
+    $res = mysqli_query($con,"select * from Person where id = '$id' ");
+    $row = mysqli_fetch_array($res);
+    
+    
+    if($row[2]<$score)
+    {
+     mysqli_query($con,"UPDATE Person SET score='$score' where id = '$id' ");
+     echo "GOOD! New Record : ",$score;
+    }
+    else
+    {
+     echo "Your Record : ",$row[2];
+    }
+    
+ }
+ 
+   
+mysqli_close($con);  
+?> 
